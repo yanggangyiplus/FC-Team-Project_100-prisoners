e27522323599b6380417ed482d50b9d91c7eea9f
SoWooJu <7053swj@naver.com>
2025-09-23 17:27:37 +0900

    feat: Print buzz

--- fizzbuzz_ulgoon.py
+++ fizzbuzz_ulgoon.py
@@ -1,5 +1,7 @@
 for i in range(1, 15+1):
-f i % 3 == 0:
-    print('fizz')
-else:
-    print(i)
+    if i % 3 == 0:
+        print('fizz')
+    elif i % 5 == 0:
+        print('buzz')
+    else:
+        print(i)
